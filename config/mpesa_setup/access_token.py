ecd00ade1eb7f6bbce20a9063d369cdb697d7ea8
Developer-felix <onjombafelix1@gmail.com>
2022-05-30 13:54:33 -0700

    first commit

---
+++ config/mpesa_setup/access_token.py
@@ -0,0 +1,14 @@
+import requests
+from requests.auth import HTTPBasicAuth
+import keys
+
+def generate_access_token():
+    #getting the access tocken
+    # consumer_key = consumer_key
+    consumer_secret_key = keys.consumer_secret_key
+    api_URL = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+    r = requests.get(api_URL, auth=HTTPBasicAuth(keys.consumer_key, consumer_secret_key))
+    json_response = r.json()
+    my_access_token = json_response["access_token"]
+    return my_access_token
+
---
+++ transactions/models.py
@@ -0,0 +1,40 @@
+import uuid
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+from django.contrib.auth.models import User
+
+
+class LNMOnline(models.Model):
+    CheckoutRequestID = models.CharField(max_length=255,blank=True,null=True)
+    MerchantRequestID = models.CharField(max_length=255,blank=True,null=True)
+    ResultCode  = models.CharField(max_length=255,blank=True,null=True)
+    ResultDesc  = models.CharField(max_length=255,blank=True,null=True)
+    MpesaReceiptNumber = models.CharField(max_length=255,blank=True,null=True)
+    Amount = models.FloatField(blank=True,null=True)
+    Balance = models.CharField(max_length=255,blank=True,null=True)
+    TransactionDate = models.DateTimeField(blank=True,null=True)
+    PhoneNumber  = models.CharField(max_length=255,blank=True,null=True)
+
+    def __str__(self):
+        return f"{self.PhoneNumber} has sent {self.Amount} >> {self.MpesaReceiptNumber}"
+
+
+STATUS = ((1, "Pending"), (0, "Complete"), (0, "Failed"))
+
+class Transaction(models.Model):
+    reciever = models.ForeignKey(User,on_delete=models.CASCADE,related_name="reciever",blank=True,null=True)
+    sender = models.ForeignKey(User,on_delete=models.CASCADE,related_name="sender",blank=True,null=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="transaction_user",blank=True,null=True)
+    transaction_id = models.CharField(max_length=255,blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True,blank=True,null=True)
+    ammount = models.IntegerField(default = 0)
+    is_withdrawal = models.BooleanField(default=False)
+    is_sending = models.BooleanField(default=True)
+
+    # def save(self, *args, **kwargs):
+    #     self.transaction_id = generate_transaction_code_id()
+    #     super(Transaction, self).save(*args, **kwargs)
+
+    class Meta:
+        db_table = "transactions_tbl"
+
